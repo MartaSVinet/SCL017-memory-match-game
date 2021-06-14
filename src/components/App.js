326a6c1bedb5c553d618669091c89818403b5497
carlamorales <carlamoralespacheco@gmail.com>
2021-06-14 09:59:00 -0400

    ediciones varias

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,30 @@ console.log(pokemon);
 
 // array de ítems (objetos).
 const dataItems = pokemon.items;
-console.log(dataItems);
 
 // array de ítems duplicados.
 const duplicatedData = dataItems.concat(dataItems);
-console.log(duplicatedData);
 
 // función para barajar duplicatedData.
 function shuffle(array) {
-  for(let i = array.length -1 ; i > 0 ;i--) {
-    let j = Math.floor( Math.random() * (i + 1) );
-    [array[i] ,array[j]] = [array[j], array[i]];
+  for(let i = array.length -1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temporary = array[i];
+    array[i] = array[j];
+    array[j] = temporary;
   }
-}    
+};
 shuffle(duplicatedData);
+
+// array que guarda las cartas cliqueadas.
+const clicked = [];
+
+// array que guarda las cartas emparejadas.
+const matchedCards = [];
+console.log(matchedCards);
+
+// variable que registra el puntaje por cartas emparejadas.
+let score = 0;
 
 const App = () => {
   // estructura html.
@@ -41,6 +51,10 @@ const App = () => {
   cardsContainer.className = "cards-container";
   main.appendChild(cardsContainer);
 
+  // elemento que muestra el contador.
+  const scoreboar = document.createElement("p");
+  main.appendChild(scoreboar);
+
   // marcos e imágenes de las cartas.
   for (let i = 0; i < duplicatedData.length; i++) {
     let cardFrames = document.createElement("div");
@@ -49,10 +63,31 @@ const App = () => {
     cardImages.setAttribute("src", "../images/back.jpg");
     cardImages.addEventListener("click", function() {
       cardImages.setAttribute("src", duplicatedData[i].image);
+      clicked.push(duplicatedData[i]);
+      checkMatch(clicked);
     });
     cardFrames.appendChild(cardImages);
     cardsContainer.appendChild(cardFrames);
   }
+
+  // función que compara los pares de cartas cliqueadas.
+  function checkMatch(arrr) {
+    while (arrr.length == 2) {
+      if (arrr[0].id == arrr[1].id) {
+        score += 100;
+        scoreboar.innerHTML = score;
+        let uno = clicked.pop(arrr[0]);
+        let dos = clicked.pop(arrr[1]);
+        matchedCards.push(uno);
+        matchedCards.push(dos);
+        alert("Match");
+      } else {
+        arrr.length = 0;
+        alert("No Match");
+      }
+      break;
+    }
+  };
 
   return generalContainer;
 };
